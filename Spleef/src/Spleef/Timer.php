7a2cebaf7bbbd2c248c94b6eb26ac57c8dbc40aa
fycarman <manuelficara01@gmail.com>
2015-08-05 01:56:33 +0200

    Update Timer.php

--- Spleef/src/Spleef/Timer.php
+++ Spleef/src/Spleef/Timer.php
@@ -17,8 +17,14 @@ public function __construct($plugin){
     $this->plugin->sec -= 1;
     if($this->plugin->sec => 20){
       foreach($this->getLevelByName($this->getConfig()->get("level"))->getPlayer() as $p){
-        $p->sendMessage("Spleef starts in ".$this->plugin->sec." seconds");
+        $p->sendMessage(TextFormat::GOLD . "Spleef starts in ".$this->plugin->sec." seconds");
       }
     }elseif($this->plugin->sec === 0){
+     if(count($this->getServer()->getLevelByName($this->getConfig()->get("level"))->getPlayers()) => 2){
+      $player->sendPopup(TextFormat::GREEN . "Spleef started! Have fun!");
+//TODO give item
+       }else{
+//TODO can't start & teleport to lobby
+    }
   }
 }
